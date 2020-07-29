d17ed1c81ee6cbb0a51b68e9a12878dac8f67f6a
jtclayt <jt.clayton92@yahoo.com>
2020-07-29 10:21:49 -0700

    Remove withdrawal limts, update transaction validators, add manager to Transaction.

--- app/models.py
+++ app/models.py
@@ -1,29 +1,34 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from datetime import datetime, date, timedelta
+from datetime import datetime
+
 
 # Validations
 class TransactionManager(models.Manager):
-    errors = {}
     # Log a purchase
     def validate_purchase(self, postData):
+        errors = {}
         todaysDate = datetime.now().date()
         if postData['date'] > todaysDate:
             errors['date'] = "Purchase must've been made today or in the past"
 
         if len(postData['desc']) < 2:
             errors['desc'] = "Description must be greater that 2 characters"
-        
+
         if not postData['amount'] > 0:
             errors['amount'] = "Amount must be greater than 0"
-    
+        return errors
+
     # Make a transfer
     def validate_transfer(self, postData):
+        errors = {}
         if not postData['amount'] > 0:
             errors['amount'] = "Amount must be greater than 0"
+        return errors
 
     # Pay a Bill
-    def validate_bill(self, postData):  
+    def validate_bill(self, postData):
+        errors = {}
         if not postData['account_number'] > 0:
             errors['account_number'] = "Account number must be greater than 0"
         if not postData['amount'] > 0:
@@ -33,9 +38,11 @@ class TransactionManager(models.Manager):
             errors['date'] = "Date must be today or a future date"
         if len(postData['note']) < 2:
             errors['note'] = "Note must be greater that 2 characters"
+        return errors
 
     # External Transfer
     def validate_extransfer(self, postData):
+        errors = {}
         if not postData['account_number'] > 0:
             errors['account_number'] = "Account number must be greater than 0"
         if not postData['amount'] > 0:
@@ -45,11 +52,14 @@ class TransactionManager(models.Manager):
             errors['date'] = "Date must be today or a future date"
         if len(postData['note']) < 2:
             errors['note'] = "Note must be greater that 2 characters"
-    
+        return errors
+
     # ATM Transaction
     def validate_atm(self, postData):
+        errors = {}
         if not postData['amount'] > 0:
             errors['amount'] = "Amount must be greater than 0"
+        return errors
 
 
 # Models
@@ -63,7 +73,6 @@ class BaseModel(models.Model):
 
 class AccountType(BaseModel):
     name = models.CharField(max_length=100)
-    withdrawal_limit = models.SmallIntegerField()
     interest_rate = models.FloatField()
 
 
@@ -107,3 +116,4 @@ class Transaction(BaseModel):
         related_name='transactions',
         on_delete=models.CASCADE
     )
+    objects = TransactionManager()
