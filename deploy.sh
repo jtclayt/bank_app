ca6dff1330522a293f59fac2492ce36c399238c0
jtclayt <jt.clayton92@yahoo.com>
2020-07-30 14:42:26 -0700

    Add deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author: Justin Clayton
+# This script automates the deploy process for a django project on AWS
+
+# Check required IP is provided
+if [ $# -ne 1 ]; then
+  echo "Provide only an IP address that the server will be hosted on" 1>&2
+  exit 1
+fi
+
+# Server setup steps
+# sudo apt-get update
+# sudo apt-get install nginx
+# sudo apt-get install python3-venv # Say yes if prompted
+# python3 -m venv venv
+# source venv/bin/activate
+# pip install -r requirements.txt
+# pip install gunicorn
+
+# Modify settings.py
+cd bank_app
+sed -i "s/DEBUG = True/DEBUG = True/" settings.py
+sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'$1\'\]/" settings.py
+sed -i "s/os\.path\.join\(BASE_DIR, \"static\"\)\,//" settings.py
+echo 'STATIC_ROOT = os.path.join(BASE_DIR, \"static/\")' >> settings.py
+
+
